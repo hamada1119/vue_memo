ee15237319a322d308608caa3131dd863b074073
hamada1119 <76606177+hamada1119@users.noreply.github.com>
2023-12-02 02:45:07 +0900

    memoアプリ

---
+++ src/components/MemoForm.vue
@@ -0,0 +1,69 @@
+<template>
+  <h2>New</h2>
+  <div><input type="text" v-model="title"></div>
+  <div><textarea v-model="content"></textarea></div>
+  <div class="center">
+      <button @click="save">SAVE</button>
+      <button @click="remove" v-if="memo.id">DELETE</button>
+  </div>
+</template>
+
+<script>
+export default {
+    name:"MemoForm",
+    props:[
+        'memo'
+    ],
+    data() {
+        return {
+            title: this.memo.title,
+            content:this.memo.content
+        }
+    },
+    methods:{
+        save(){
+            /*vueの知識*/
+            let memo = {
+                title:this.title,
+                content:this.content
+            }
+
+        if (this.memo.id){
+            memo.id=this.memo.id
+        }
+            this.$store.commit('save',memo)
+            this.$router.push('/')
+        },
+        remove(){
+            this.$store.commit('delete',this.memo.id)
+            this.$router.push('/')
+        }
+    }
+}
+</script>
+
+ 
+<style scoped>
+div {
+    margin-bottom: 10px;
+}
+input[type=text]{
+    width: 100%;
+    font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
+}
+textarea{
+    width: 100%;
+    height: 30em;
+    margin:3px;
+}
+
+button {
+    width: 5em;
+    margin: 3px;
+}
+
+.center {
+    text-align:center;
+}
+
+</style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,53 @@
+<template>
+  <div class="home">
+    <ul v-if="hasMemos">
+      <li v-for="memo in memos" :key="memo.id">
+        <router-link :to="{name: 'edit',params: {id: memo.id}}">
+        {{ memo.title }}
+        </router-link>
+      </li>
+    </ul>
+    <p v-else>
+      You don't have memos<br>
+      Pleage write Memo!!!
+    </p>
+  </div>
+</template>
+
+<style scoped>
+ul {
+  margin: 0;
+  padding: 0;
+}
+
+li {
+  list-style: none;
+  border-bottom: 1px solid #ccc;
+  padding-bottom: 10px;
+  margin-bottom: 10px;
+}
+
+li a {
+  color: #999;
+  text-decoration: none;
+  width: 100%;
+  display: block;
+}
+
+</style>
+
+
+<script>
+export default {
+  name:'HomeView',
+  computed: {
+    hasMemos() {
+      return this.$store.getters.getCount
+    },
+    memos() {
+      return this.$store.state.memos
+    }
+  }
+}
+</script>
+
